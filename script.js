c9339c7f8c3e07e9466af71742c83bfeeda9beb9
VonyaC <64337036+VonyaC@users.noreply.github.com>
2023-04-08 11:42:44 -0400

    removed test files

--- script.js
+++ script.js
@@ -2,11 +2,7 @@ $(document).ready(() => {
     let ctrlIcon = $("#ctrlIcon");
     let playBtn = $("#playBtn")
     let song = $("#song")[0];
-    // let songProgress = setInterval(()=>{
-    //     $("#progress-bar").progressbar({
-    //         value: song.currentTime
-    //       });
-    //   }, 500);
+
 
     playBtn.click((evt) => {
         if (ctrlIcon.hasClass("fa-pause")) {
@@ -18,7 +14,7 @@ $(document).ready(() => {
             ctrlIcon.hasClass("fa-play");
             ctrlIcon.addClass("fa-pause");
             ctrlIcon.removeClass("fa-play");
-            setInterval(()=>{
+            setInterval(() => {
                 $("#progress-bar").slider({
                     value: song.currentTime
                 });
@@ -26,26 +22,16 @@ $(document).ready(() => {
         }
 
     });
-    // if (song.play()){
-    //   setInterval(()=>{
-    //     $("#progress-bar").progressbar({
-    //         value: song.currentTime
-    //       });
-    //   }, 500);
-    // }
 
-    // $("#progress-bar").click(() => {
-        $("#progress-bar").slider({
-            max: song.duration,
-            slide: (e, ui)=> {
-                song.currentTime = ui.value
-                song.play(); 
-                ctrlIcon.addClass('fa-pause')
-                ctrlIcon.removeClass('fa-play')
-            }
-        });
-
-    // });    
+    $("#progress-bar").slider({
+        max: song.duration,
+        slide: (e, ui) => {
+            song.currentTime = ui.value
+            song.play();
+            ctrlIcon.addClass('fa-pause')
+            ctrlIcon.removeClass('fa-play')
+        }
+    });
 
 
 });
